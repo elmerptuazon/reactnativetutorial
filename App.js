54230ddc94a38574b78f95e4c471de8e84d3ddde
Elmer <elmerptuazon@yahoo.com>
2019-02-08 15:31:45 +0800

    alert and js debugger

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import {Alert, Platform, StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,12 +21,15 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-        inputText: "**Elmer"
+        username: "Username",
+        password: ""
     };
     this.state.customStyles = {
         color: 'red'
     };
-    this.handleChangeText = this.handleChangeText.bind(this)
+    this.handleUsernameChangeText = this.handleUsernameChangeText.bind(this)
+    this.handlePasswordChangeText = this.handlePasswordChangeText.bind(this)
+    this.handleButtonPressed = this.handleButtonPressed.bind(this)
 
     setInterval(() => {
         if(this.state.customStyles.color == 'red') {
@@ -44,12 +47,29 @@ export default class App extends Component {
     }, 1000);
   }
 
-  handleChangeText(newText) {
-    this.setState({
-        inputText: newText
-    })
-  }
+    handleUsernameChangeText(newText) {
+      this.setState({
+          username: newText
+      })
+    }
 
+    handlePasswordChangeText(newText) {
+      this.setState({
+          password: newText
+      })
+    }
+
+    handleButtonPressed() {
+        Alert.alert(
+            'Alert Title',
+            'My Alert Msg',
+            [
+            {text: 'Cancel', onPress: () => console.log('Cancelled')},
+            {text: 'OK', onPress: () => console.log('Ok Pressed')}
+            ],
+            {cancelable: true}
+        )
+    }
   render() {
     const pic = {
                 uri: 'https://vignette.wikia.nocookie.net/creepypasta/images/e/e2/Anime-Girl-With-Silver-Hair-And-Purple-Eyes-HD-Wallpaper.jpg/revision/latest?cb=20140120061808'
@@ -57,15 +77,16 @@ export default class App extends Component {
     return (
         <View>
             <View style={styles.container}>
-                <Text style={[styles.welcome, this.state.customStyles]}>Welcome {this.state.inputText} to React Native!</Text>
+                <Text style={[styles.welcome, this.state.customStyles]}>Welcome {this.state.username} to React Native!</Text>
             </View>
             <View style={styles.containerBody}>
                 <Text>Username</Text>
-                <TextInput defaultValue={this.state.inputText} onChangeText={this.handleChangeText}/>
+                <TextInput defaultValue={this.state.username} onChangeText={this.handleUsernameChangeText}/>
                 <Text>Password</Text>
-                <TextInput defaultValue={this.state.inputText} onChangeText={this.handleChangeText}/>
+                <TextInput defaultValue={this.state.password} onChangeText={this.handlePasswordChangeText}/>
+                <Button title={"Submit"} onPress={this.handleButtonPressed} />
             </View>
-            <Image source={pic} style={{flexGrow:1,alignItems:'center',width:null, height:null}} />
+            <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:150}} />
         </View>
     );
   }
