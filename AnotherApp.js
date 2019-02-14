7a6b3815fb691256ade5ade4b09059eaf845585a
Elmer <elmerptuazon@yahoo.com>
2019-02-14 18:01:45 +0800

    react navigation phase

---
+++ AnotherApp.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Alert, Platform, ActivityIndicator, ScrollView, StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
+
+
+export default class AnotherApp extends Component {
+    render() {
+        return (
+            <Text>Second Page Here</Text>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    containerBody: {
+        justifyContent: 'center',
+        padding: 20
+    },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
--- App.js
+++ App.js
@@ -11,12 +11,20 @@ import React, {Component} from 'react';
 import {Alert, Platform, ActivityIndicator, ScrollView, StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import NavigationBar from 'react-native-navbar';
+import { StackNavigator } from 'react-navigation';
+import AnotherApp from './AnotherApp';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Hello World,\n',
 });
+
+const AnotherPage = StackNavigator({
+        Home: {
+            screen: AnotherApp
+        }
+    });
 
 
 export default class App extends Component {
@@ -33,6 +41,7 @@ export default class App extends Component {
     this.handleUsernameChangeText = this.handleUsernameChangeText.bind(this)
     this.handlePasswordChangeText = this.handlePasswordChangeText.bind(this)
     this.handleButtonPressed = this.handleButtonPressed.bind(this)
+
 
     setInterval(() => {
         if(this.state.customStyles.color == 'red') {
@@ -62,10 +71,12 @@ export default class App extends Component {
       })
     }
 
+
     handleButtonPressed() {
         this.setState({
             loading: true
         })
+
         setTimeout(() => {
             Alert.alert(
                         'Alert Title',
@@ -76,6 +87,10 @@ export default class App extends Component {
                         ],
                         {cancelable: true}
                     )
+            this.setState({
+                loading: false
+            })
+
         }, 5000)
     }
   render() {
@@ -86,11 +101,13 @@ export default class App extends Component {
       title: 'Next',
       handler: () => alert('Next Page'),
     };
+
     return (
         <View style={styles.container}>
             <View>
-                <NavigationBar title={{title:'NavBar Title'}} style={{color:'white', backgroundColor:'blue'}} rightButton={rightButtonConfig}/>
+                <NavigationBar title={{title:'NavBar Title'}} style={{ backgroundColor:'blue'}} rightButton={rightButtonConfig}/>
                 <Text style={[styles.welcome, this.state.customStyles]}>Welcome {this.state.username} to React Native!</Text>
+                <AnotherPage />
             </View>
             <ScrollView>
             <View style={styles.containerBody}>
@@ -99,13 +116,13 @@ export default class App extends Component {
                 <Text>Password</Text>
                 <TextInput defaultValue={this.state.password} onChangeText={this.handlePasswordChangeText}/>
                 <Button title={"Submit"} onPress={this.handleButtonPressed} />
-                <ActivityIndicator size="large" color="#0000ff" />
+                <View><ActivityIndicator style={{opacity: this.state.loading ? 1.0 : 0.0}} animating={true} size="large" color="#0000ff" /></View>
                 <Text>{"\n"}</Text>
                 <Icon.Button name="facebook" backgroundColor="#3b5998">Login with FB</Icon.Button>
-                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }}  /><Text style={{width:100, height:100, fontWeight: 'italic', fontSize:20,}}>Image Content</Text>
-                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontWeight: 'italic', fontSize:20,}}>Image Content</Text>
-                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontWeight: 'italic', fontSize:20,}}>Image Content</Text>
-                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontWeight: 'italic', fontSize:20,}}>Image Content</Text>
+                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }}  /><Text style={{width:100, height:100, fontStyle:'italic', fontSize:20,}}>Image Content</Text>
+                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontStyle:'italic', fontSize:20,}}>Image Content</Text>
+                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontStyle:'italic', fontSize:20,}}>Image Content</Text>
+                <Image source={pic} style={{alignItems:'center',margin:20, width:100, height:100, borderRadius: 40, overflow:'hidden' }} /><Text style={{width:100, height:100, fontStyle:'italic', fontSize:20,}}>Image Content</Text>
 
             </View>
             </ScrollView>
@@ -129,14 +146,4 @@ const styles = StyleSheet.create({
     color: 'black',
     backgroundColor: 'pink'
   },
-    activityIndicatorWrapper: {
-      backgroundColor: '#FFFFFF',
-      height: 100,
-      width: 100,
-      borderRadius: 10,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      overflow:'hidden'
-    }
 });
